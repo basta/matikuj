939ce9196216856b0bb5e6ff1f920dd581d89637
Viki <viki@localhost.localdomain>
2021-01-22 19:13:15 +0100

    creating question base setup

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from app import app
 from flask import render_template, redirect
 
 from .forms import SampleForm
+from .create_question import CreateQuestionForm
 
 
 @app.route('/')
@@ -14,6 +15,16 @@ def index():
 @app.route('/articles')
 def articles():
     return render_template('articles.html')
+
+
+@app.route('/create-question', methods=['GET', 'POST'])
+def create_question():
+    form = CreateQuestionForm()
+    # If form is upon submission and is validated success
+    if form.validate_on_submit():
+        return redirect('index')
+
+    return render_template('create-question.html', form=form)
 
 
 @app.route('/forms', methods=['GET', 'POST'])
